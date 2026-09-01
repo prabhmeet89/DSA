4530096670297663764bc2852b6b788d7bb308bf
prabhmeet singh <meet97094@gmail.com>
2026-09-01 13:17:13 +0530

    Time: 1187 ms (40.82%) | Memory: 399.4 MB (31.63%) - LeetSync

---
+++ 3870-minimum-moves-to-clean-the-classroom/minimum-moves-to-clean-the-classroom.cpp
@@ -0,0 +1,117 @@
+class Solution {
+public:
+    int minMoves(vector<string>& classroom, int energy) {
+        
+        int m = classroom.size();
+        int n = classroom[0].size();
+
+        int sr = -1, sc = -1;
+
+        // Give every litter a number
+        vector<vector<int>> litterId(m, vector<int>(n, -1));
+        int litterCount = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (classroom[i][j] == 'S') {
+                    sr = i;
+                    sc = j;
+                }
+
+                if (classroom[i][j] == 'L') {
+                    litterId[i][j] = litterCount++;
+                }
+            }
+        }
+
+        int targetMask = (1 << litterCount) - 1;
+
+        // visited[r][c][energy][mask]
+        vector<vector<vector<vector<bool>>>> visited(
+            m,
+            vector<vector<vector<bool>>>(
+                n,
+                vector<vector<bool>>(
+                    energy + 1,
+                    vector<bool>(1 << litterCount, false)
+                )
+            )
+        );
+
+        // r, c, currentEnergy, mask
+        queue<array<int, 4>> q;
+
+        q.push({sr, sc, energy, 0});
+        visited[sr][sc][energy][0] = true;
+
+        int moves = 0;
+
+        int dr[] = {-1, 1, 0, 0};
+        int dc[] = {0, 0, -1, 1};
+
+        while (!q.empty()) {
+
+            int sz = q.size();
+
+            while (sz--) {
+
+                auto [r, c, currEnergy, mask] = q.front();
+                q.pop();
+
+                // All litter collected
+                if (mask == targetMask) {
+                    return moves;
+                }
+
+                for (int d = 0; d < 4; d++) {
+
+                    int nr = r + dr[d];
+                    int nc = c + dc[d];
+
+                    // Outside grid
+                    if (nr < 0 || nr >= m || nc < 0 || nc >= n)
+                        continue;
+
+                    // Obstacle
+                    if (classroom[nr][nc] == 'X')
+                        continue;
+
+                    // Need energy to make a move
+                    if (currEnergy == 0)
+                        continue;
+
+                    int newEnergy = currEnergy - 1;
+                    int newMask = mask;
+
+                    // Collect litter
+                    if (classroom[nr][nc] == 'L') {
+                        int id = litterId[nr][nc];
+                        newMask |= (1 << id);
+                    }
+
+                    // Reset energy
+                    if (classroom[nr][nc] == 'R') {
+                        newEnergy = energy;
+                    }
+
+                    if (!visited[nr][nc][newEnergy][newMask]) {
+
+                        visited[nr][nc][newEnergy][newMask] = true;
+
+                        q.push({
+                            nr,
+                            nc,
+                            newEnergy,
+                            newMask
+                        });
+                    }
+                }
+            }
+
+            moves++;
+        }
+
+        return -1;
+    }
+};
