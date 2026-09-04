d33227e219d1533a88dd184439358e16149f5e3c
prabhmeet singh <meet97094@gmail.com>
2026-09-04 15:29:06 +0530

    Time: 0 ms (100.00%) | Memory: 31.1 MB (66.39%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -3,24 +3,25 @@ public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
 
+        int suffixMin = nums[n - 1];
+
+        // Find suffix minimums in-place is not allowed,
+        // so use right[].
         vector<int> right(n);
 
-        // right[i] = minimum from i to n-1
         right[n - 1] = nums[n - 1];
 
-        for (int i = n - 2; i >= 0; i--) {
-            right[i] = min(right[i + 1], nums[i]);
+        for (int i = n - 2; i >= 0; --i) {
+            right[i] = min(nums[i], right[i + 1]);
         }
 
-        int left = 0;
+        int prefixMax = nums[0];
 
-        // left = maximum from 0 to i
-        for (int i = 0; i < n; i++) {
-            left = max(left, nums[i]);
+        for (int i = 0; i < n; ++i) {
+            prefixMax = max(prefixMax, nums[i]);
 
-            if (left - right[i] <= k) {
+            if (prefixMax - right[i] <= k)
                 return i;
-            }
         }
 
         return -1;
