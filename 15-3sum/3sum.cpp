115e04cf626a4c555ec2f14cf26bfc313491d0c4
prabhmeet singh <meet97094@gmail.com>
2026-09-15 01:25:54 +0530

    Time: 39 ms (94.63%) | Memory: 29.2 MB (28.49%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,57 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-
         vector<vector<int>> ans;
-
         sort(nums.begin(), nums.end());
 
-        int n = nums.size();
+        for (int i = 0; i < nums.size() - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (nums[i] > 0) break;
 
-        for (int i = 0; i < n - 2; i++) {
+            int l = i + 1, r = nums.size() - 1;
 
-            // Skip duplicate first numbers
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
 
                 if (sum == 0) {
+                    ans.push_back({nums[i], nums[l], nums[r]});
 
-                    ans.push_back({
-                        nums[i],
-                        nums[left],
-                        nums[right]
-                    });
+                    while (l < r && nums[l] == nums[l + 1]) l++;
+                    while (l < r && nums[r] == nums[r - 1]) r--;
 
-                    left++;
-                    right--;
-
-                    // Skip duplicate left values
-                    while (left < right &&
-                           nums[left] == nums[left - 1]) {
-                        left++;
-                    }
-
-                    // Skip duplicate right values
-                    while (left < right &&
-                           nums[right] == nums[right + 1]) {
-                        right--;
-                    }
+                    l++;
+                    r--;
                 }
-
-                else if (sum < 0) {
-                    left++;
-                }
-
-                else {
-                    right--;
-                }
+                else if (sum < 0) l++;
+                else r--;
             }
         }
 
