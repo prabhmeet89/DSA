f0196694ceda0fee8f6db5a8ca8b83082f3ef60a
prabhmeet singh <meet97094@gmail.com>
2026-09-21 13:48:21 +0530

    Time: 3 ms (23.56%) | Memory: 9 MB (17.66%) - LeetSync

---
+++ 60-permutation-sequence/permutation-sequence.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string helper(string str, int k, string ans) {
+        int n = str.size();
+
+        if(n == 1) {
+            return ans + str[0];
+        }
+
+        int fact = 1;
+
+        for(int i = 2; i <= n - 1; i++) {
+            fact *= i;
+        }
+
+        int idx = k / fact;
+
+        char ch = str[idx];
+
+        string left = str.substr(0, idx);
+        string right = str.substr(idx + 1);
+
+        k = k % fact;
+
+        return helper(left + right, k, ans + ch);
+    }
+
+    string getPermutation(int n, int k) {
+        string str = "";
+
+        for(int i = 1; i <= n; i++) {
+            str += to_string(i);
+        }
+
+        k--;  // convert to 0-indexed
+
+        return helper(str, k, "");
+    }
+};
